fd23eefb14078ab5f02c0070697f0d33b4856a37
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-06-21 01:12:01 +0330

    wrong item is corrected

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -64,8 +64,9 @@ public class Menu {
         String password = scanner.next();
         if (username.startsWith("1")) {
             personService.findByUsernameAndPasswordForAdmin(username, password);
-            System.out.println("librarian***");
-            System.out.println(userLibrarian.getName());
+            System.out.println("------------------------------------------------");
+            System.out.println("Hello Admin your welcome . . . . ");
+            System.out.println("librarian: " + userLibrarian.getName() + " " + userLibrarian.getFamily());
             runProgramForAdmin();
         } else {
             personService.findByUsernameAndPasswordForMember(username, password);
@@ -81,7 +82,6 @@ public class Menu {
         System.out.println();
         System.out.println("1)assortment of subject          2)assortment of book");
         System.out.println();
-        System.out.println("************************************************************");
         System.out.println();
         switch (scanner.nextInt()) {
             case 1:
@@ -178,52 +178,95 @@ public class Menu {
     public void menuForBook() {
         System.out.println("===========******  assortment book   ******===========");
         System.out.println("1)Add book");
-        System.out.println("2)Edit subject of book");
-        System.out.println("3)remove book");
-        System.out.println("4)load All  book");
+        System.out.println("2)load All  book with Edit subject and remove");
         System.out.println("************************************************************");
-        System.out.println("5)assortment of subject          6)back to main menu");
-      manageBook();
+        System.out.println("3)assortment of subject          4)back to main menu");
+        manageBook();
     }
 
     public void manageBook() {
         while (true) {
             switch (scanner.nextInt()) {
                 case 1:
-                   Set<Subject> subjectSet=new HashSet<>(subjectService.loadAll());
+                    Set<Subject> subjectSet = new HashSet<>(subjectService.loadAll());
                     System.out.println("please select one subject from list ");
-                    for (Subject subject : subjectSet){
-                        System.out.println("this is subject: " +subject);
+                    for (Subject subject : subjectSet) {
+                        System.out.println("this is subject: " + subject);
                         System.out.println("do you want select it or no");
-                        System.out.println("1)s -> select    2)n -> next subject");
-                        if (scanner.next().equals("s")){
+                        System.out.println("s -> select    n -> next subject");
+                        if (scanner.next().equals("s")) {
                             System.out.println("inter author of this book");
-                            String author=scanner.next();
+                            String author = scanner.next();
                             System.out.println("inter your print year");
-                            int printYear=scanner.nextInt();
-                            Book book=new Book(subject,author,printYear);
+                            int printYear = scanner.nextInt();
+                            Book book = new Book(subject, author, printYear);
                             bookService.save(book);
+                            System.out.println("saved . . .");
                             break;
                         }
 
-                }
-                   break;
+                    }
+                    menuForBook();
+                    break;
                 case 2:
+                    Set<Book> bookSetForEdit = new HashSet<>(bookService.loadAll());
+                    if (bookSetForEdit.size()>0) {
+
+                        boolean flag = true;
+                        while (flag) {
+                            for (Book book : bookSetForEdit) {
+
+                                System.out.println("--------------------------------------------------------------------------");
+                                System.out.println("subject: " + book.getSubject().getTitle() + "author: " + book.getAuthor() + "print year: " + book.getPrintYear());
+                                System.out.println("--------------------------------------------------------------------------");
+                                System.out.println("1)Edit subject                 2)remove                 3)next book");
+                                switch (scanner.nextInt()) {
+                                    case 1:
+                                        Set<Subject> subjectSetForEdit = new HashSet<>(subjectService.loadAll());
+                                        System.out.println("please select one subject from list ");
+                                        for (Subject subject : subjectSetForEdit) {
+                                            System.out.println("this is subject: " + subject.getTitle());
+                                            System.out.println("do you want select it or no");
+                                            System.out.println("s -> select    n -> next subject");
+                                            if (scanner.next().equals("s")) {
+                                                book.setSubject(subject);
+                                                bookService.update(book);
+                                                System.out.println("book subject is updated . . .");
+                                                break;
+                                            }
+                                        }
+                                        break;
+                                    case 2:
+                                        System.out.println("Are you sure that remove this book? y ->yes  n ->no");
+                                        if (scanner.next().equals("y")) {
+                                            bookService.remove(book);
+                                            System.out.println("removed . . .");
+                                        }
+                                        break;
+                                    case 3:
+                                        if (bookSetForEdit.size() == 1)
+                                            System.out.println("number of book is one and not found another");
+                                        System.out.println();
+                                        break;
+                                }
+
+                                System.out.println("for going to menu of manage book press e");
+                                if (scanner.next().equals("e")) {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }else  System.out.println("number of book is zero and not found");
+                    menuForBook();
                     break;
                 case 3:
-                    break;
-                case 4: Set<Book> bookSet=new HashSet<>(bookService.loadAll());
-                    for (Book book:bookSet
-                         ) {
-                        System.out.println(book);
-                    }
-                    break;
-                case 5:
                     menuForSubject();
                     break;
-                case 6:
+                case 4:
                     runProgramForAdmin();
                     break;
+
             }
         }
     }
